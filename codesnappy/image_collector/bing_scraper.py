7e349e46e2b78620ab3b6c9b18ee833b2125e460
= <jj@vmjh.com>
2021-06-01 15:35:16 +0100

    housekeeping

--- codesnappy/image_collector/bing_scraper.py
+++ codesnappy/image_collector/bing_scraper.py
@@ -1,10 +1,14 @@
 from bing_image_downloader import downloader
-downloader.download(query_string, limit=100,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, print_out=True)
 
-class PycharmScraper:
-    def __init__(self, input_search):
+
+class BingScraper:
+    def __init__(self, input_search, limit, output_dir):
         self.input_search = input_search
-        self.output = self.scrape_bing
+        self.output = self.scrape_bing()
 
     def scrape_bing(self):
-        downloader.download(self.input_search, limit=100000,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, print_out=True))
+        download_things = downloader.download(self.input_search, limit=100,  output_dir='dataset',
+                                              adult_filter_off=True,
+                                              force_replace=False, timeout=60)
+        return download_things
+
